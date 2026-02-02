1f6d9db028dc61292ea4581fe53009a82c98a307
FLÁVIO OLIVEIRA FERREIRA <123007905+BondfinderBR@users.noreply.github.com>
2026-02-02 19:07:58 -0300

    Delete experiments/operational directory

--- experiments/operational/Expe1/emissor.py
+++
@@ -1,8 +0,0 @@
-# emissor.py
-import random
-
-def emitir():
-    # decide por qual fenda "saiu"
-    escolha = random.choice(["A", "B"])
-    print(f"[EMISSOR] saiu pela fenda {escolha}")
-    return escolha
--- experiments/operational/Expe1/observer_log.py
+++
@@ -1,6 +0,0 @@
-# observer_log.py
-def registrar(real, inferido):
-    print("------ RESULTADO ------")
-    print(f"Real: {real}")
-    print(f"Inferido: {inferido}")
-    print("-----------------------")
